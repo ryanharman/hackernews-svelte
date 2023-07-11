5c814bfe8fa34b9428b7f798b78c7a43229db3ca
Ryan Harman <ryan.harman63@gmail.com>
2023-07-11 12:28:18 +0100

    Recursive comments

--- src/components/comment.svelte
+++ src/components/comment.svelte
@@ -11,11 +11,17 @@
 </script>
 
 <div>
-    <h3 class="font-medium text-sm">{comment.user} {comment.time_ago} <button class="ml-1" on:click={toggleOpen}>[-]</button></h3>
-    {#if isCommentOpen}
-      <div class="font-light text-md" contenteditable="inherit" bind:innerHTML={comment.content} />
-      <slot />
-      {:else} 
-      <button class="hover:underline font-light text-sm text-slate-600" on:click={toggleOpen}>Read comment</button>
-    {/if}
+  <h3 class="font-medium text-sm">{comment.user} {comment.time_ago} <button class="ml-1" on:click={toggleOpen}>[-]</button></h3>
+  {#if isCommentOpen}
+    <div class="font-light text-md" contenteditable="inherit" bind:innerHTML={comment.content} />
+      <div class="ml-6 space-y-4 mt-4">
+        {#if comment.comments && comment.comments.length > 0}
+          {#each comment.comments as nestedComment}
+            <svelte:self comment={nestedComment} />
+          {/each}
+        {/if}
+      </div>
+    {:else} 
+    <button class="hover:underline font-light text-sm text-slate-600" on:click={toggleOpen}>Read comment</button>
+  {/if}
 </div>
--- src/routes/post/[id]/+page.svelte
+++ src/routes/post/[id]/+page.svelte
@@ -9,15 +9,7 @@
 {#if data.item.comments && data.item.comments.length > 0}
   {#each data.item.comments as comment}
     <div class="mb-6">
-      <Comment {comment} >
-        <div class="ml-6 space-y-4 mt-4">
-          {#if comment.comments && comment.comments.length > 0}
-            {#each comment.comments as nestedComment}
-            <Comment comment={nestedComment} />
-            {/each}
-          {/if}
-        </div>
-      </Comment>    
+      <Comment {comment} />
     </div>
   {/each}
 {/if}
