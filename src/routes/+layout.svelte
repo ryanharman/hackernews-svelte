f4a3a8e09d3873513535ecb3dbea4d68f86a3d0e
Ryan Harman <ryan.harman63@gmail.com>
2025-01-12 17:25:08 +0000

    chore: backlink to main site

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,9 +5,16 @@
 <main
   class="flex flex-col justify-start items-center p-16 bg-zinc-100 min-h-screen text-zinc-800"
 >
-  <a href="/" class="pb-6">
-    <h1 class="text-xl font-bold">Hacker News</h1>
-  </a>
+  <div class="flex gap-4 items-center pb-6">
+    <a href="/">
+      <h1 class="text-xl font-bold">Hacker News</h1>
+    </a>
+
+    <a href="https://ryanharman.dev" class="pt-1 text-zinc-500">
+      <h2 class="text-sm">by Ryan Harman</h2>
+    </a>
+  </div>
+
   <div class="max-w-3xl w-full">
     <slot />
   </div>
